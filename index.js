65c5614a6bda61f280a9178d5066b103a1e0c341
Edlyn Shih <edlyn0418@gmail.com>
2022-09-11 23:15:23 -0400

    Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
-    console.log("It didn't work!" , error);
+    console.log("It didn't work!", error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
-  
+  console.log('It worked! Returned IP:', ip);
+
+
   fetchCoordsByIP(ip, (error, coordinates) => {
     if (error) {
       console.log("It didn't work!", error.message);
       return;
     }
-  
+
     console.log("It worked! Returned coordinates: ", coordinates);
+
+    fetchISSFlyOverTimes(coordinates, (error, flytimes) => {
+      if (error) {
+        console.log("It didn't work!", error.message);
+        return;
+      }
+
+      console.log("It worked! Returned flying times: ", flytimes);
+
+    });
 
   });
 });
